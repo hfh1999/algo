7b98e5c19f1a54875c79577d0556d217d2e6b447
hfh1999 <2350827470@qq.com>
2019-06-21 10:45:46 +0800

    linux 3th day:calculator almost complete the fuction to build a (primary) grammar tree.

--- tree_p/calculator/generateT.c
+++ tree_p/calculator/generateT.c
@@ -1,35 +1,34 @@
 #include <stdio.h>
 #include <string.h>
-#include "tree.h"
 #include "getWord_Opt.h"
-#define MAX 100
+#include "buildTree.h"
+#define MAX_EXPR 100
 int main (void)
 {
     
 }
 void grammarTree(char * str)
 {
-    char expr[MAX];
+    char expr[MAX_EXPR];
     strcpy(expr,str);
 
-    BTnode * bulidP;
-    BTnode * tree;
+    Gtree tree;
     word_opt * item;
     Expr *  expression  = initW_O(str); // create a struct "W_O"
     while( getW_O(expression,item))
     {
         static int prev = 0; // previous priority
         BTnode * tree;
-        if(( tree = inittree()) ==  NULL)
+        if(inittree(&tree) == 0)
             printf("error:Init grammar tree.\n");
         BTnode * buildP = tree; //buildP is a tool pointer;
 
         if (item->opt.priority >= prev )
-            treeGdown(tree,buildP,item);
+            treeGdown(&tree,*item);
         else if(item->opt.priority == NONE)
-            treeExtra(tree,bulidP,item);
+            treeExtra(&tree,*item);
         else
-            treeGup(tree,buildP,item);
+            treeGup(&tree,*item);
         prev = item->opt.priority;
     }
 }
